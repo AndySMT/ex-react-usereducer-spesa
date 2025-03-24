91e8c68e8fafbada8626a42a47fc38dd02053000
AndySMT <andyiulian.simota@gmail.com>
2025-03-24 17:19:55 +0100

    📌 Milestone 2: Aggiungere prodotti al carrello

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,42 @@ function App() {
   const [products, setProducts] = useState([
     { name: "Mela", price: 0.5 },
     { name: "Pane", price: 1.2 },
-    { name: "Latte", price: 1.0 },
+    { name: "Latte", price: 1.1 },
     { name: "Pasta", price: 0.7 },
   ]);
+
+  const [addedProducts, setAddedProducts] = useState([]);
+  function addToCart(product) {
+    const prodotto = addedProducts.find((prod) => prod.name === product.name);
+    if (!prodotto) {
+      setAddedProducts([...addedProducts, { ...product, quantity: 1 }]);
+    }
+  }
+
   return (
     <>
       <ul>
         {products.map((product, index) => {
           return (
-            <li key={index}>
+            <li key={index} className="p-1">
               Nome: {product.name} - Prezzo: {product.price}
+              <button
+                onClick={() => addToCart(product)}
+                className="bg-blue-500 rounded px-1 ml-2"
+              >
+                Aggiungi al carrello
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <h1 className="text-red-600">Carrello</h1>
+      <ul>
+        {addedProducts.map((prod, index) => {
+          return (
+            <li key={index}>
+              Nome: {prod.name} - Prezzo: {prod.price} - Quantità:{" "}
+              {prod.quantity}
             </li>
           );
         })}
